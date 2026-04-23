5d28908736b0d612757408f9f87bd8bb797ffb51
Nawal KIshor <98528821+guddu7677@users.noreply.github.com>
2026-04-23 10:12:55 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,25 +1,17 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const authMiddleware = require("../middlewares/authMiddleware");
-// const roleMiddleware = require("../middlewares/roleMiddleware");
-// const { createUserByAdmin } = require("../controllers/adminController");
-
-// router.post(
-//   "/create-user",
-//   authMiddleware,
-//   roleMiddleware("admin"),
-//   createUserByAdmin
-// );
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../middlewares/authMiddleware");
+const roleMiddleware = require("../middlewares/roleMiddleware");
 const { createUserByAdmin } = require("../controllers/adminController");
 
-// 🔥 TEMPORARY (no auth)
-router.post("/create-user", createUserByAdmin);
+router.post(
+  "/create-user",
+  authMiddleware,
+  roleMiddleware("admin"),
+  createUserByAdmin
+);
 
 module.exports = router;
+
+
